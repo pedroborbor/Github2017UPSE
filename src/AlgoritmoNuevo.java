f565ca67e0787574ac39fa98c6a120e9dadb7ac5
peter <pedritoap9@hotmail.com>
2017-07-19 11:15:04 -0500

    agregado archivo de proyecto

--- src/AlgoritmoNuevo.java
+++ src/AlgoritmoNuevo.java
@@ -3,7 +3,24 @@ public class AlgoritmoNuevo {
 
 	public static void main(String[] args) {
 		System.out.println("algoirtfdjhbcn");
+		int i= 0;
+		int nota=0;
+		boolean copiaEnELExamen= true;
+		do{
+			System.out.println("tu notaes "+nota);
+			if(i>5){
+				break;
+			}
+			i++;
+		}while(copiaEnELExamen);
 
+	}
+	
+	public static void imprimirTesis(){
+		System.out.println("imprimir");
+		System.out.println("mas lata");
+		System.out.println("mas lata++");
+		
 	}
 
 }
